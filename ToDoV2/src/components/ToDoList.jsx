d3f6c32376f431e791cde3a395d3d0ee5ec5ffee
Daniel Pawłowicz <danielpawlowicz@interia.pl>
2024-09-20 10:49:10 +0200

    Initial components

---
+++ ToDoV2/src/components/ToDoList.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Task from './Task'
+import Form from './Form'
+
+const ToDoList = () => {
+  return (
+    <>
+        <Form />
+        <Task />
+    </>
+  )
+}
+
+export default ToDoList
