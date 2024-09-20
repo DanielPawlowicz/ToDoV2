d7f4f30194e1261d19b5c6c5de9c20a56f665101
Daniel Pawłowicz <danielpawlowicz@interia.pl>
2024-09-20 18:01:48 +0200

    collapsing subtasks and showing hamburger icon on hover

--- ToDoV2/src/components/Task.jsx
+++ ToDoV2/src/components/Task.jsx
@@ -1,12 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './Task.module.css';
+import {FaAngleDown, FaAngleUp, FaBars} from "react-icons/fa";
 
-const Task = ({ task, isSub }) => {
+const Task = ({ task, isSub = false }) => {
+
+  const [showSubtasks, setShowSubtasks] = useState(false);
+  const [isHovered, setIsHovered] = useState(false);
+
+  const handleMouseEnter = () => {
+    setIsHovered(true);
+  };
+
+  const handleMouseLeave = () => {
+    setIsHovered(false);
+  };
+
   return (
-    <li className={isSub ? styles.subtask : styles.superiorTask}>
-      <input type="checkbox" className={isSub ? styles.checkboxSub : styles.checkboxSup} />
-      <span>{task.title}</span>
-      {task.subtasks && (
+    <li 
+      className={isSub ? styles.subtask : styles.superiorTask}
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+    >
+      <div className={styles.inlineContainer}>
+        <div className={styles.secondContainer}>
+          <input type="checkbox" className={isSub ? styles.checkboxSub : styles.checkboxSup} />
+          <span className={task.isChecked ? `${styles.title_checked} ${styles.task_title}` : styles.task_title }>{task.title}</span>
+          {task.subtasks && (
+            <button className={styles.toggle_button} onClick={() => setShowSubtasks(!showSubtasks)}>
+              {showSubtasks ? <FaAngleUp/> : <FaAngleDown />}
+            </button>
+          )}
+        </div>
+        {isHovered && (
+          <div className={styles.barsContainer}>
+            <FaBars />
+          </div>
+        )}
+      </div>
+      {task.subtasks && showSubtasks && (
         <ul>
           {task.subtasks.map((subtask) => (
             <Task key={subtask.id} task={subtask} isSub={true}/>
--- ToDoV2/src/components/TasksList.jsx
+++ ToDoV2/src/components/TasksList.jsx
@@ -3,11 +3,12 @@ import Task from './Task'
 import styles from './TasksList.module.css'
 
 const TasksList = ({ tasks }) => {
+
   return (
     <>
         <ul>
             {tasks.map((task) => (
-                <Task key={task.id} task={task}/>
+                <Task key={task.id} task={task} />
             ))}
         </ul>
     </>
