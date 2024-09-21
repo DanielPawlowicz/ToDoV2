9936f247eb7ba7b95f97b38fff6f62663fbec5c2
Daniel Pawłowicz <danielpawlowicz@interia.pl>
2024-09-21 10:52:30 +0200

    Fetching data from json server

--- ToDoV2/src/components/ToDoList.jsx
+++ ToDoV2/src/components/ToDoList.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Task from './Task'
 import Form from './Form'
 import taskList from '../assets/tasks.json'
@@ -7,8 +7,24 @@ import styles from './ToDoList.module.css'
 
 const ToDoList = () => {
 
-    const [tasks, setTasks] = useState(taskList);
-    console.log(tasks);
+    // const [tasks, setTasks] = useState(taskList);
+    // console.log(tasks);
+    
+    const [tasks, setTasks] = useState([]);
+    
+    useEffect(()=>{
+      const fetchTasks = async () => {
+        try {
+          const res = await fetch('http://localhost:8000/toDoList1');
+          const data = await res.json();
+          setTasks(data);
+        } catch (error) {
+          console.error("Error fetching data: " + error)
+        }
+      }
+
+      fetchTasks();
+    },[]);
 
   return (
     <div className={styles.container}>
