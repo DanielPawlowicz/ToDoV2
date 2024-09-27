f9a27d60aad3d9a5dfc61324ff487d1269e37e2e
Daniel Pawłowicz <danielpawlowicz@interia.pl>
2024-09-27 12:50:14 +0200

    focus form again after adding new task

--- ToDoV2/src/components/Form.jsx
+++ ToDoV2/src/components/Form.jsx
@@ -1,36 +1,49 @@
-import React, { useState } from 'react'
-import styles from './Form.module.css'
+import React, { useState, useRef, useEffect } from 'react';
+import styles from './Form.module.css';
 
-const Form = ({addTask}) => {
+const Form = ({ addTask }) => {
 
   const [newTaskTitle, setNewTaskTitle] = useState('');
+  const inputRef = useRef(null);
 
-  const newTaskSubmit = (e) =>{
 
+  const newTaskSubmit = (e) => {
     e.preventDefault();
 
     const newTask = {
       id: crypto.randomUUID(),
       title: newTaskTitle,
-      isChecked: false
-    }
-
-    console.log(newTask);
+      isChecked: false,
+    };
 
     addTask(newTask);
-
-    const input = document.getElementById("title_input");
-    input.value = '';
-
-  }
+    setNewTaskTitle('');
+  };
 
 
+  // Focus the input element after every render
+  useEffect(() => {
+    if (inputRef.current) {
+      inputRef.current.focus();
+    }
+  }, [newTaskTitle]);
+
+  
   return (
-    <form onSubmit={(newTaskSubmit)}>
-      <input id="title_input" type='text' className='form_title' name='form_title' size={50} onChange={(e) => setNewTaskTitle(e.target.value)}/>
-      <button>Add Task</button> 
+    <form onSubmit={newTaskSubmit}>
+      <input
+        id="title_input"
+        type="text"
+        className="form_title"
+        name="form_title"
+        size={50}
+        value={newTaskTitle}
+        onChange={(e) => setNewTaskTitle(e.target.value)}
+        ref={inputRef}
+      />
+      <button type="submit">Add Task</button>
     </form>
-  )
-}
+  );
+};
 
-export default Form
+export default Form;
